e29c033cb53ebc505da39c974570e8413a4217f6
Huan <flamex9365@gmail.com>
2026-06-15 08:28:37 +0700

    feat: add MongoDB database connection configuration and remove obsolete documentation file

--- config/database.js
+++ config/database.js
@@ -2,11 +2,14 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
+    if (!process.env.MONGO_URL) {
+      console.error("MONGO_URL is NOT defined in environment variables.");
+      return;
+    }
     await mongoose.connect(process.env.MONGO_URL);
     console.log("MongoDB Connected");
   } catch (error) {
-    console.log(error);
-    process.exit(1);
+    console.error("Database Connection Error: ", error);
   }
 };
 
